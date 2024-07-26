899968657cfffde47e896d2a045fb07cd2ee7c04
matytyma <matytyma22@gmail.com>
2024-07-26 20:30:18 +0200

    Use Kotlin DSL for build scripts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,37 @@
 plugins {
-    id 'org.jetbrains.kotlin.jvm' version '2.0.0'
+    kotlin("jvm") version "2.0.0"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+    application
+    idea
 }
 
-group = 'dev.matytyma'
-version = '1.0-SNAPSHOT'
+group = "dev.matytyma"
+version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation 'org.jetbrains.kotlin:kotlin-test'
+    implementation("dev.kord:kord-core:0.14.0")
+    implementation("org.slf4j:slf4j-simple:2.0.13")
+    implementation("io.github.cdimascio:dotenv-kotlin:6.4.1")
+    testImplementation(kotlin("test"))
 }
 
-test {
+tasks.test {
     useJUnitPlatform()
 }
+
+application {
+    mainClass = "dev.matytyma.MainKt"
+}
+
 kotlin {
-    jvmToolchain(21)
+    jvmToolchain(17)
+}
+
+idea.module {
+    isDownloadJavadoc = true
+    isDownloadSources = true
 }
